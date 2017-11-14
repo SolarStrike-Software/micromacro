39f62bd7b28f289025b0d96d6d03660c98904e00
katherine@oceanexecutive.com <katherine@oceanexecutive.com>
2017-11-13 22:59:00 -0600

    Version number change to 1.96

--- src/version.h
+++ src/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "01";
-	static const char MONTH[] = "10";
+	static const char DATE[] = "13";
+	static const char MONTH[] = "11";
 	static const char YEAR[] = "2017";
-	static const char UBUNTU_VERSION_STYLE[] =  "17.10";
+	static const char UBUNTU_VERSION_STYLE[] =  "17.11";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR  = 1;
-	static const long MINOR  = 95;
+	static const long MINOR  = 96;
 	static const long BUILD  = 1;
-	static const long REVISION  = 1009;
+	static const long REVISION  = 1010;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1714;
-	#define RC_FILEVERSION 1,95,1,1009
-	#define RC_FILEVERSION_STRING "1, 95, 1, 1009\0"
-	static const char FULLVERSION_STRING [] = "1.95.1.1009";
+	static const long BUILDS_COUNT  = 1753;
+	#define RC_FILEVERSION 1,96,1,1010
+	#define RC_FILEVERSION_STRING "1, 96, 1, 1010\0"
+	static const char FULLVERSION_STRING [] = "1.96.1.1010";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 123;
+	static const long BUILD_HISTORY  = 124;
 	
 
 }
