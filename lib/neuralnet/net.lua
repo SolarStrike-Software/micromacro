a20985b62bb35797458ed504d49e3cddc93db0af
katherine@oceanexecutive.com <katherine@oceanexecutive.com>
2018-02-16 19:19:01 -0600

    Neural network fixes

--- lib/neuralnet/net.lua
+++ lib/neuralnet/net.lua
@@ -12,6 +12,7 @@ function NeuralNet:constructor(...)
 	end
 
 	self.layers	=	{};
+	self.recentAverageError = 1.0;
 
 	local numLayers	=	#topology;
 	for i = 1,numLayers do
@@ -25,9 +26,7 @@ function NeuralNet:constructor(...)
 		end
 
 		-- Fill the layer's neurons, add a bias neuron in each layer
-		--printf("Neurons for layer: %d, outputs: %d\n", topology[i], numOutputs);
 		for index = 1, topology[i]+1 do
-			--print("Inserting new neuron", index, "into layer", i, "Outputs", numOutputs);
 			table.insert(newLayer, Neuron(index, numOutputs));
 		end
 
@@ -36,7 +35,13 @@ function NeuralNet:constructor(...)
 	end
 end
 
-function NeuralNet:feed(inputs)
+function NeuralNet:feed(...)
+	local inputs;
+	if( type(...) == "table" ) then
+		inputs = ...;
+	else
+		inputs = {...};
+	end
 	assert(#inputs == #self.layers[1] - 1, "Number of inputs does not match network topology.");
 
 	-- Set input values into first layer
@@ -44,32 +49,47 @@ function NeuralNet:feed(inputs)
 		self.layers[1][i]:setOutputValue(v);
 	end
 
+
+
 	-- Propagate forward
 	for i = 2,#self.layers do
 		local prevLayer	=	self.layers[i - 1];
-		for j,k in pairs(self.layers[i]) do
+		for j = 1, #self.layers[i] - 1 do
+			local k = self.layers[i][j];
 			k:feed(prevLayer);
 		end
 	end
 end
 
 function NeuralNet:backPropagation(...)
-	local targets = {...};
+	local targets;
+	if( type(...) ~= "table" ) then
+		targets = {...};
+	else
+		targets = ...;
+	end
 	table.insert(targets, 1.0); -- Insert a 1.0 for the bias neuron
 
 	-- Calculate RMS
 	local layer	=	self.layers[#self.layers];
 	local rms	=	0.0;
 
-	for i = 1,#layer do -- Iterate over each neuron in the last layer
+	for i = 1,#layer-1 do -- Iterate over each neuron in the last layer except bias
 		local delta = targets[i] - layer[i]:getOutputValue();
 		rms = rms + delta * delta;
+	end
+	rms = rms / (#layer-1);
+	rms = math.sqrt(rms);
 
+	-- Recent average RMS
+	local recentAverageSmoothing = 100.0;
+	self.recentAverageError = (self.recentAverageError * recentAverageSmoothing + rms) / (recentAverageSmoothing + 1.0);
+	
+	for i = 1,#layer-1 do
 		-- Calculate gradient while we're at it
 		layer[i]:calculateOutputGradients(targets[i]);
 	end
-	rms = math.sqrt(rms / (#layer-1));
-	
+
 	-- Calculate hidden layer gradients
 	for i = #self.layers - 1, 2, -1 do
 		local hiddenLayer	=	self.layers[i];
@@ -86,10 +106,16 @@ function NeuralNet:backPropagation(...)
 		local layer		=	self.layers[l];
 		local prevLayer	=	self.layers[l-1];
 
-		for i,v in pairs(layer) do -- Iterate over each neuron
-			v:updateInputWeights(prevLayer);
+		for i = 1, #layer - 1 do
+			layer[i]:updateInputWeights(prevLayer);
 		end
 	end
+
+	table.remove(targets); -- Pop last element to remove the bias neuron
+end
+
+function NeuralNet:getRecentAverageError()
+	return self.recentAverageError;
 end
 
 function NeuralNet:getResults()
@@ -129,9 +155,9 @@ function NeuralNet:save(filename)
 	local export = self:getExportTable();
 
 	file:write("return {");
-	for i,v in pairs(export) do
+	for i = 1, #export do
 		file:write("{");
-		for j,k in pairs(v) do
+		for j,k in pairs(export[i]) do
 			file:write("{");
 			for n,m in pairs(k) do
 				if( n < #k ) then
@@ -140,7 +166,7 @@ function NeuralNet:save(filename)
 					file:write(sprintf("%f", m));
 				end
 			end
-			if( j < #v ) then
+			if( j < #export[i] ) then
 				file:write("},");
 			else
 				file:write("}");
@@ -159,8 +185,18 @@ end
 
 -- Load from a file
 function NeuralNet:load(filename)
-	print(filesystem.getCWD() .. "/" .. filename);
-	local import = dofile(filesystem.getCWD() .. "/" .. filename);
+	local file = io.open(filename, "r");
+
+	if( not file ) then
+		error(sprintf("Could not open `%s` for reading.", filename), 2);
+	end
+	local data = file:read("*all");
+	local chunk = load(data);
+	local status, import = pcall(chunk);
+
+	if( not status ) then
+		error(sprintf("`%s` does not appear to be a valid neural network table", filename), 2);
+	end
 
 	if( type(import) ~= "table" ) then
 		error("File does not contain a valid table.", 2);
@@ -174,8 +210,8 @@ function NeuralNet:load(filename)
 	self:constructor(topology); -- Recreate network topology
 
 	-- Step through, reassign weights
-	for i,layer in pairs(self.layers) do
-		for j,neuron in pairs(layer) do
+	for i = 1, #self.layers-1 do
+		for j,neuron in pairs(self.layers[i]) do
 			for n,synapse in pairs(neuron.synapses) do
 				synapse.weight = import[i][j][n];
 			end
--- lib/neuralnet/neuron.lua
+++ lib/neuralnet/neuron.lua
@@ -21,7 +21,6 @@ function Neuron:setOutputValue(value)
 end
 
 function Neuron:getOutputWeight(index)
-	--print("Getting weight for", index);
 	return self.synapses[index].weight;
 end
 
@@ -30,15 +29,14 @@ function Neuron:getGradient()
 end
 
 function Neuron:feed(prevLayer)
-	-- Step 1: Sum up previous layer's outputs
+
+	-- Step 1: Sum up previous layer's outputs, including bias
 	local sum	=	0.0;
 
 	for i,v in pairs(prevLayer) do
-		--print("\t", i, v);
 		sum	=	sum +
 			v:getOutputValue() *
 			v:getOutputWeight(self.index);
-			--v.synapses[self.index].weight;
 	end
 
 	self.value	=	self:transferFunction(sum);
@@ -55,52 +53,23 @@ function Neuron:calculateHiddenGradients(nextLayer)
 end
 
 function Neuron:updateInputWeights(prevLayer)
-	local	eta		=	0.15;	-- Overall Net learning rate, should be in range [0..1]
-	local	alpha	=	0.5;	-- Momentum, multiple of last deltaWeight, should be in range [0..1]
+	local	eta		=	0.3;	-- Overall Net learning rate, should be in range [0..1]
+	local	alpha	=	0.7;	-- Momentum, multiple of last deltaWeight, should be in range [0..1]
 
 	for i,v in pairs(prevLayer) do
 		local prevLayerNeuron	=	v;
 		local oldDeltaWeight	=	prevLayerNeuron.synapses[self.index].deltaWeight;
 
-		local newDeltaWeight	=	eta * prevLayerNeuron:getOutputValue() * self.gradient + alpha  * oldDeltaWeight;
+		local newDeltaWeight	=	eta * prevLayerNeuron:getOutputValue() * self.gradient + alpha * oldDeltaWeight;
+		local newWeight			=	prevLayerNeuron.synapses[self.index].weight + newDeltaWeight;
+		--newWeight = math.min(math.max(newWeight, 0.0), 1.0);
 		prevLayerNeuron.synapses[self.index].deltaWeight	=	newDeltaWeight;
-		prevLayerNeuron.synapses[self.index].weight			=	prevLayerNeuron.synapses[self.index].weight + newDeltaWeight;
+		prevLayerNeuron.synapses[self.index].weight			=	newWeight;
 	end
 end
 
 function Neuron:transferFunction(x)
-	-- tanh() implementation
-	if( x == 0 ) then
-		return 0.0;
-	end
-
-	local neg = false;
-
-	if( x < 0 ) then
-		x = -x;
-		neg = true;
-	end
-
-	if( x < 0.54930614433405 ) then
-		local y = x * x;
-		x = x + x * y *
-		((-0.96437492777225469787e0  * y +
-		  -0.99225929672236083313e2) * y +
-		  -0.16134119023996228053e4) /
-		(((0.10000000000000000000e1  * y +
-		   0.11274474380534949335e3) * y +
-		   0.22337720718962312926e4) * y +
-		   0.48402357071988688686e4);
-	else
-		x = math.exp(x);
-		x = 1.0 - 2.0 / (x * x + 1.0);
-	end
-
-	if( neg ) then
-		x = -x;
-	end
-
-	return x;
+	return math.tanh(x);
 end
 
 function Neuron:transferFunctionDerivative(x)
@@ -111,7 +80,7 @@ function Neuron:sumDOW(nextLayer)
 	-- Sum of contribution to errors at the nodes we feed from this neuron
 	local sum = 0.0;
 	for i,v in pairs(nextLayer) do
-		sum	=	sum + self.synapses[i].weight * nextLayer[i]:getGradient();
+		sum = sum + (self.synapses[i].weight * nextLayer[i]:getGradient());
 	end
 
 	return sum;
