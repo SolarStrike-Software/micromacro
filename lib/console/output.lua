75c9330cad3065a38268c4c351f279fe5a45ea13
elverion <admin@solarstrike.net>
2022-02-19 20:36:51 -0400

    Console output library

---
+++ lib/console/output.lua
@@ -0,0 +1,101 @@
+ConsoleOutput = class.new()
+
+
+
+
+function ConsoleOutput:constructor()
+    self.useAnsi = true
+    self.ansiAvailable = self:isAnsiAvailable()
+
+    self.styles = {
+        ['default'] ='%s',
+        ['petty'] = "\x1b[90m%s\x1b[0m",
+        ['info'] = "\x1b[96m%s\x1b[0m",
+        ['comment'] = "\x1b[33m%s\x1b[0m",
+        ['success'] = "\x1b[32m%s\x1b[0m",
+        ['fail'] = "\x1b[31m%s\x1b[0m",
+        ['warning'] = "\x1b[1;30;43m%s\x1b[0;0m",
+        ['error'] = "\x1b[1;97;41m%s\x1b[0;0m",
+    }
+end
+
+function ConsoleOutput:setStyle(style, fmt)
+    self.styles[style] = fmt
+end
+
+function ConsoleOutput:isAnsiAvailable()
+    if( os.getenv('NO_COLOR') ) then
+        return false
+    end
+
+    if( os.getenv('TERM_PROGRAM') == 'Hyper' ) then
+        return true
+    end
+
+    if( io.stdout == nil ) then
+        return false
+    end
+
+    return true;
+end
+
+function ConsoleOutput:write(msg)
+    io.write(msg)
+end
+
+function ConsoleOutput:writeln(msg)
+    self:write(msg or '')
+    self:write("\n")
+end
+
+function ConsoleOutput:sstyle(style, msg)
+    if( not(self.useAnsi and self.ansiAvailable) or self.styles[style] == nil ) then
+        return msg
+    end
+
+    return sprintf(self.styles[style], msg)
+end
+
+function ConsoleOutput:style(style, msg)
+    self:write(self:sstyle(style, msg))
+end
+
+function ConsoleOutput:default(msg)
+    self:style('default', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:petty(msg)
+    self:style('petty', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:info(msg)
+    self:style('info', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:comment(msg)
+    self:style('comment', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:success(msg)
+    self:style('success', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:fail(msg)
+    self:style('fail', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:warning(msg)
+    self:style('warning', msg)
+    self:writeln()
+end
+
+function ConsoleOutput:error(msg)
+    self:style('error', msg)
+    self:writeln()
+end
---
+++ lib/console/progressbar.lua
@@ -0,0 +1,116 @@
+require 'console/output'
+ConsoleProgressBar = class.new()
+
+local output = ConsoleOutput()
+output:setStyle('filledbar', "\x1b[32m%s\x1b[0m")
+output:setStyle('unfilledbar', "\x1b[90m%s\x1b[0m")
+
+local function saveCursorPosition()
+    io.write('\x1b[s')
+end
+
+local function restoreCursorPosition()
+    io.write('\x1b[u')
+end
+
+function ConsoleProgressBar:constructor(min, max, width)
+    self.min = min or 0
+    self.max = max or 100
+    self.current = self.min
+    self.width = width or 20
+
+    self.showPercent = true
+    self.showRaw = true
+    self.showBar = true
+
+    self.startFmt = '['
+    self.endFmt = ']'
+    self.emptyChar = '█'
+    self.fullChar = '█'
+
+    self.minRedrawTime = 0.5
+    self.lastDrawTime = time.getNow()
+    self.lastDrawRatio = 0
+    self.began = false
+    self.dirty = false
+end
+
+getmetatable(ConsoleProgressBar).__gc = function ()
+    self:finish()
+end
+
+function ConsoleProgressBar:begin()
+    saveCursorPosition()
+    self.began = true
+end
+
+function ConsoleProgressBar:getRatio()
+    return (math.min(self.current, self.max) - self.min) / (self.max - self.min)
+end
+
+function ConsoleProgressBar:advance(amount)
+    if( not self.began ) then
+        self:begin()
+    end
+
+    self.current = self.current + (amount or 1)
+    local ratio = self:getRatio()
+
+    if( (ratio - self.lastDrawRatio) > 0.01 ) then
+        self.dirty = true
+    end
+
+    self:update()
+end
+
+function ConsoleProgressBar:finish()
+    self:draw()
+    io.write('\n')
+    self.began = false
+end
+
+function ConsoleProgressBar:draw()
+    restoreCursorPosition()
+    local ratio = self:getRatio()
+
+
+    local result = ''
+    local percentStr = ''
+    local rawStr = ''
+    local barStr = ''
+
+    if( self.showRaw ) then
+        local maxLen = string.len(math.ceil(self.max))
+        local totalLen = maxLen*2 + 1
+        rawStr = sprintf('%-' .. totalLen .. 's', sprintf('%d/%d', self.current, self.max))
+        result = result .. rawStr .. ' '
+    end
+
+    if( self.showBar ) then
+        local filledBlocks = math.round(ratio*self.width)
+        barStr = self.startFmt
+            .. output:sstyle('filledbar', string.rep(self.fullChar, filledBlocks))
+            .. output:sstyle('unfilledbar', string.rep(self.emptyChar, self.width - filledBlocks))
+            .. self.endFmt
+
+        result = result .. barStr .. ' '
+    end
+
+    if( self.showPercent ) then
+        percentStr = sprintf('%3d%%', math.round(ratio * 100))
+        result = result .. percentStr .. ' '
+    end
+
+    io.write(result)
+
+    self.lastDrawTime = time.getNow()
+    self.lastDrawRatio = ratio
+end
+
+function ConsoleProgressBar:update()
+    if( not self.dirty and time.diff(self.lastDrawTime) < self.minRedrawTime ) then
+        return
+    end
+
+    self:draw()
+end
