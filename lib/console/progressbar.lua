c8f6cc9a170c27fe5488ef2da2de9da7c9412ae7
elverion <elverion@users.noreply.github.com>
2024-04-02 13:46:45 -0300

    Refactoring, add new Ocean Wave progress bar style

--- lib/console/progressbar.lua
+++ lib/console/progressbar.lua
@@ -1,52 +1,7 @@
 require 'console/output'
-ConsoleProgressBarStyle = class.new()
+require 'console/progressbar_style'
+
 ConsoleProgressBar = class.new()
-
-function ConsoleProgressBarStyle:constructor()
-    self.showPercent = true
-    self.showRaw = true
-    self.showBar = true
-
-    self.startFmt = '['
-    self.endFmt = ']'
-    self.emptyChar = '#'
-    self.fullChar = '#'
-
-    self.filledBarFmt = "\x1b[38;5;82m%s\x1b[0m"
-    self.unfilledBarFmt = "\x1b[38;5;235m%s\x1b[0m"
-end
-
---[[
-    Get the styling for the full block at a specific position (index from the left side of the bar).
-    `width` will also be passed, and is the total width of the progress bar in characters.
-
-    You may override this function to provide per-character styling.
-    See RainbowConsoleProgressBarStyle:getFilledStyle() for an example
-]]
-function ConsoleProgressBarStyle:getFilledStyle(position, width)
-    return self.filledBarFmt
-end
-
-SolidGreenConsoleProgressBarStyle = ConsoleProgressBarStyle()
-SolidGreenConsoleProgressBarStyle.fullChar = ' '
-SolidGreenConsoleProgressBarStyle.emptyChar = ' '
-SolidGreenConsoleProgressBarStyle.filledBarFmt = "\x1b[38;5;41;48;5;46m%s\x1b[0m"
-SolidGreenConsoleProgressBarStyle.unfilledBarFmt = "\x1b[38;5;1m%s\x1b[0m"
-
-RainbowConsoleProgressBarStyle = ConsoleProgressBarStyle()
-RainbowConsoleProgressBarStyle.fullChar = ' '
-RainbowConsoleProgressBarStyle.emptyChar = ' '
-RainbowConsoleProgressBarStyle.filledBarFmt = "\x1b[38;5;1;48;5;1m%s\x1b[0m"
-RainbowConsoleProgressBarStyle.unfilledBarFmt = "\x1b[38;5;1;48;5;234m%s\x1b[0m"
-
-function RainbowConsoleProgressBarStyle:getFilledStyle(position, width)
-    local ratio = position / width
-    local colors = {160, 166, 172, 178, 184, 190, 154, 118, 82, 84, 85, 48, 42, 36, 30, 24, 27, 25, 67, 62, 57}
-    local index = math.floor(ratio * #colors) + 1
-    local color = colors[math.min(index, #colors)]
-
-    return "\x1b[38;5;233;48;5;" .. sprintf("%d", color) .. "m%s\x1b[0m"
-end
 
 local function saveCursorPosition()
     io.write('\x1b[s')
@@ -63,7 +18,7 @@ function ConsoleProgressBar:constructor(min, max, width, style)
     self.width = width or 20
     self.style = style or ConsoleProgressBarStyle();
 
-    self.minRedrawTime = 0.5
+
     self.lastDrawTime = time.getNow()
     self.lastDrawRatio = 0
     self.began = false
@@ -111,7 +66,6 @@ function ConsoleProgressBar:draw()
     local result = ''
     local percentStr = ''
     local rawStr = ''
-    local barStr = ''
 
     if (self.style.showRaw) then
         local maxLen = string.len(math.ceil(self.max))
@@ -121,19 +75,22 @@ function ConsoleProgressBar:draw()
     end
 
     if (self.style.showBar) then
-        local filledBlocks = math.round(ratio * self.width)
-        local unfilledBlocks = self.width - filledBlocks
+        local filledWidth = math.round(ratio * self.width)
+        local unfilledWidth = self.width - filledWidth
 
         local filledBlocksStr = ''
-        for i = 1, filledBlocks do
-            filledBlocksStr = filledBlocksStr .. sprintf(self.style:getFilledStyle(i, self.width), self.style.fullChar)
+        for i = 1, filledWidth do
+            local char = self.style:getFilledChar(i, filledWidth, self.width)
+            filledBlocksStr = filledBlocksStr .. sprintf(self.style:getFilledStyle(i, filledWidth, self.width), char)
         end
 
-        barStr = self.style.startFmt .. filledBlocksStr ..
-                     sprintf(self.style.unfilledBarFmt, string.rep(self.style.emptyChar, unfilledBlocks)) ..
-                     self.style.endFmt
+        local unfilledBlocksStr = ''
+        for i = 1, unfilledWidth do
+            local char = self.style:getUnfilledChar(i, filledWidth, self.width)
+            unfilledBlocksStr = unfilledBlocksStr .. sprintf(self.style:getUnfilledStyle(i, filledWidth, self.width), char)
+        end
 
-        result = result .. barStr .. ' '
+        result = result .. self.style.startFmt .. filledBlocksStr .. unfilledBlocksStr .. self.style.endFmt .. ' '
     end
 
     if (self.style.showPercent) then
@@ -148,7 +105,7 @@ function ConsoleProgressBar:draw()
 end
 
 function ConsoleProgressBar:update()
-    if (not self.dirty and time.diff(self.lastDrawTime) < self.minRedrawTime) then
+    if (not self.dirty and time.diff(self.lastDrawTime) < (self.style.minRedrawTime or 0.5)) then
         return
     end
 
---
+++ lib/console/progressbar_style.lua
@@ -0,0 +1,93 @@
+ConsoleProgressBarStyle = class.new()
+
+function ConsoleProgressBarStyle:constructor()
+    self.showPercent = true
+    self.showRaw = true
+    self.showBar = true
+
+    self.startFmt = '['
+    self.endFmt = ']'
+    self.emptyChar = '#'
+    self.fullChar = '#'
+
+    self.filledBarFmt = "\x1b[38;5;82m%s\x1b[0m"
+    self.unfilledBarFmt = "\x1b[38;5;235m%s\x1b[0m"
+
+    self.minRedrawTime = 0.5
+end
+
+--[[
+    Get the styling for the full block at a specific position (index from the left side of the bar).
+    `filledWidth` is the width, in characters, of the filled section of the bar
+    `totalWidth` is the total width (filled & unfilled) of the progress bar in characters.
+
+    You may override this function to provide per-character styling.
+    See RainbowConsoleProgressBarStyle:getFilledStyle() for an example
+]]
+function ConsoleProgressBarStyle:getFilledStyle(position, filledWidth, totalWidth)
+    return self.filledBarFmt
+end
+
+--[[
+    Get the styling for the empty block at a specific position (index from the left side of the bar).
+    `filledWidth` is the width, in characters, of the filled section of the bar
+    `totalWidth` is the total width (filled & unfilled) of the progress bar in characters.
+
+    You may override this function to provide per-character styling.
+]]
+function ConsoleProgressBarStyle:getUnfilledStyle(position, filledWidth, totalWidth)
+    return self.unfilledBarFmt
+end
+
+--[[
+    Get a character to use for a filled block of the progress bar.
+    Defaults to the fullChar set on the style, but can be overridden to get more control
+]]
+function ConsoleProgressBarStyle:getFilledChar(position, filledWidth, totalWidth)
+    return self.fullChar
+end
+
+--[[
+    Get a character to use for a unfilled block of the progress bar.
+    Defaults to the emptyChar set on the style, but can be overridden to get more control
+]]
+function ConsoleProgressBarStyle:getUnfilledChar(position, filledWidth, totalWidth)
+    return self.emptyChar
+end
+
+DefaultConsoleProgressBarStyle = ConsoleProgressBarStyle()
+
+SolidGreenConsoleProgressBarStyle = ConsoleProgressBarStyle()
+SolidGreenConsoleProgressBarStyle.fullChar = ' '
+SolidGreenConsoleProgressBarStyle.emptyChar = ' '
+SolidGreenConsoleProgressBarStyle.filledBarFmt = "\x1b[38;5;41;48;5;46m%s\x1b[0m"
+SolidGreenConsoleProgressBarStyle.unfilledBarFmt = "\x1b[38;5;1m%s\x1b[0m"
+
+RainbowConsoleProgressBarStyle = ConsoleProgressBarStyle()
+RainbowConsoleProgressBarStyle.fullChar = ' '
+RainbowConsoleProgressBarStyle.emptyChar = ' '
+
+function RainbowConsoleProgressBarStyle:getFilledStyle(position, filledWidth, totalWidth)
+    local ratio<const> = position / totalWidth
+    local colors<const> = {160, 166, 172, 178, 184, 190, 154, 118, 82, 84, 85, 48, 42, 36, 30, 24, 27, 25, 67, 62, 57}
+    local index<const> = math.floor(ratio * #colors) + 1
+    local color<const> = colors[math.min(index, #colors)]
+
+    return "\x1b[38;5;233;48;5;" .. sprintf("%d", color) .. "m%s\x1b[0m"
+end
+
+OceanWaveConsoleProgressBarStyle = ConsoleProgressBarStyle()
+OceanWaveConsoleProgressBarStyle.filledChar = '#'
+OceanWaveConsoleProgressBarStyle.emptyChar = ' '
+OceanWaveConsoleProgressBarStyle.unfilledBarFmt = "\x1b[38;5;1;48;5;234m%s\x1b[0m"
+OceanWaveConsoleProgressBarStyle.minRedrawTime = 0.1
+
+function OceanWaveConsoleProgressBarStyle:getFilledStyle(position, filledWidth, totalWidth)
+    local i64DiminishFactor<const> = 16776960 -- time.getNow() returns really big numbers; we need to scale it down a lot!
+    local timeOffset<const> = math.round(math.sin(time.getNow() / i64DiminishFactor) * 10)
+    local colors <const> = {21, 25, 26, 27, 31, 32, 33, 37, 38, 39}
+    local color <const> = (timeOffset + position) % (#colors - 1) + 1
+
+    return "\x1b[38;5;" .. sprintf("%d", colors[color]) .. ";48;5;234m%s\x1b[0m"
+end
+
